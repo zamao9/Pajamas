710ea6d8414404b3a4b68218238b079fe332eb76
zamao9 <94758143+zamao9@users.noreply.github.com>
2022-02-10 22:44:39 +0300

    dfhgh

--- js/pajamas.js
+++ js/pajamas.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  bannerCollapseHandle();
+});
+
+function bannerCollapseHandle () {
+  let miniatures = document.getElementsByClassName('miniature'),
+    mainBanner = document.getElementById('main_banner');
+
+  for (let i = 0; miniatures.length > i; ++i) {
+    miniatures[i].addEventListener('click', (event) => {
+      let src = event.currentTarget.src;
+      mainBanner.src = src;
+    })
+  }
+}
+
+
+
+
 $(function () {
 
                   /* BURGER */
@@ -8,6 +27,19 @@ $(function () {
     $(this).toggleClass('active');
     $('.nav').toggleClass('active');
   })
+
+
+
+                  /* SIZES */
+
+  $('.sizes').click(function(event) {
+    event.preventDefault();
+
+    $('.size_body').toggleClass('active');
+    $('.size_items').toggleClass('active');
+  })
+
+
 
 
 
