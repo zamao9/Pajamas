66ed79fae748f19242838199cd8e5de9a97e3273
zamao9 <94758143+zamao9@users.noreply.github.com>
2022-02-10 17:06:15 +0300

    ghj

--- js/pajamas.js
+++ js/pajamas.js
@@ -1,20 +1,12 @@
 $(function () {
 
-                  /* HEADER */
-
-  $('.header').click(function(event) {
-    event.preventDefault();
-
-    $('.nav').toggleClass('active');
-  })
-
-
                   /* BURGER */
 
   $('.burger').click(function(event) {
     event.preventDefault();
 
     $(this).toggleClass('active');
+    $('.nav').toggleClass('active');
   })
 
 
